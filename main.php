cd3bf506b15c6cdfa64e914022caca6b51b1179e
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-09-30 14:12:30 +0200

    Add files via upload

--- main.php
+++ main.php
@@ -12,6 +12,17 @@ $domeDuTonnere[] = new Humain("Pierre");
 $domeDuTonnere[] = new Orc("Chloé");
 $domeDuTonnere[] = new Elfe("Valentin");
 $domeDuTonnere[] = new Humain("Wahid");
+$domeDuTonnere[] = new Orc("Jordan");
+$domeDuTonnere[] = new Elfe("Ezster");
+$domeDuTonnere[] = new Humain("Nicolas");
+$domeDuTonnere[] = new Orc("Carla");
+$domeDuTonnere[] = new Elfe("Ludovic");
+$domeDuTonnere[] = new Humain("Lolita");
+$domeDuTonnere[] = new Orc("Élodie");
+$domeDuTonnere[] = new Elfe("Joao");
+$domeDuTonnere[] = new Humain("Yassine");
+$domeDuTonnere[] = new Elfe("Enzo");
+
 
 $game = new GameEngine();
 foreach ($domeDuTonnere as $fighter) {
